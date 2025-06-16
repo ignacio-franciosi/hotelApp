0e9fd225324d76c950ce24c7d004080d34a70ea0
Ignacio Franciosi <2104338@ucc.edu.ar>
2025-06-16 22:18:09 +0000

    Updated priceLowerThanOne.cy.js

--- HotelCrudAngular/cypress/e2e/priceLowerThanOne.cy.js
+++ HotelCrudAngular/cypress/e2e/priceLowerThanOne.cy.js
@@ -4,7 +4,12 @@ describe('AddHotel - Validación de precio mayor a 0', () => {
     const testHotelName = 'Hotel Cypress Test Add Hotel with price 0';
 
     beforeEach(() => {
-        baseUrl = Cypress.env('baseUrl') || 'http://localhost:4200/';
+        baseUrl = Cypress.env('baseUrl');
+    
+        if (!baseUrl) {
+            baseUrl = 'http://localhost:4200/';
+        }
+
         cy.visit(baseUrl);
         cy.wait(500);
     });
