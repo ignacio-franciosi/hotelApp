89b1065569311bf92758b97efe96fd98f49315b6
Ignacio Franciosi <2104338@ucc.edu.ar>
2025-06-16 22:17:27 +0000

    Updated deleteHotel.cy.js

--- HotelCrudAngular/cypress/e2e/deleteHotel.cy.js
+++ HotelCrudAngular/cypress/e2e/deleteHotel.cy.js
@@ -4,10 +4,15 @@ describe('| Tests - Delete Hotel |', () => {
     });
 
     let baseUrl;
+    let apiUrl;
 
     before(() => {
         // Crear hotel directamente por API (puedes omitir si ya existe en BD)
-        cy.request('POST', 'http://localhost:7150/api/hotel/create', {
+        apiUrl = Cypress.env('apiUrl');
+        if(!apiUrl){
+            apiUrl = 'http://localhost:7150/'
+        }
+        cy.request('POST', `${apiUrl}api/hotel/create`, {
             name: 'Hotel Cypress Test Delete',
             price: 100,
             rooms: 5,
@@ -15,14 +20,15 @@ describe('| Tests - Delete Hotel |', () => {
         });
     });
 
-     beforeEach(() => {
+    beforeEach(() => {
         baseUrl = Cypress.env('baseUrl');
+    
         if (!baseUrl) {
             baseUrl = 'http://localhost:4200/';
         }
-        cy.wait(1000);
-        cy.visit(`${baseUrl}`);
-        cy.wait(500); // tiempo para asegurar que cargue
+
+        cy.visit(baseUrl);
+        cy.wait(500);
     });
 
     it('Debería eliminar un hotel al hacer clic en el icono de eliminar', () => {
