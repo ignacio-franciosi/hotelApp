5f551f5bddafddbbbcca4b7885cd9221983aa3d6
Ignacio Franciosi <2104338@ucc.edu.ar>
2025-06-17 12:45:59 +0000

    Updated environment.prod.ts

--- HotelCrudAngular/src/environments/environment.prod.ts
+++ HotelCrudAngular/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiUrl: (typeof window !== 'undefined' && window.env && window.env.apiUrl)
       ? window.env.apiUrl
-      : 'http://localhost:7150/api/Hotel'  // URL de la API para prod
+      //: 'http://localhost:7150/api/Hotel'  // URL de la API para prod
+      : 'https://hotel-back-qa.azurewebsites.net/api/hotel'
 };
